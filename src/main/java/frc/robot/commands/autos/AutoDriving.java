f70f5ea950b98033f6a4180118a8f95a482dd602
Shassh U <imshassh@gmail.com>
2025-02-14 18:05:03 -0800

    commented out autodriving
    Co-Authored-By: William Hoang <171635909+billionhonk@users.noreply.github.com>
    Co-Authored-By: Derrick K <91711641+Paradoxicaly@users.noreply.github.com>
    Co-Authored-By: silverx257 <109992135+silverx257@users.noreply.github.com>

--- src/main/java/frc/robot/commands/autos/AutoDriving.java
+++ src/main/java/frc/robot/commands/autos/AutoDriving.java
@@ -1,49 +1,49 @@
-package frc.robot.commands.autos;
+// package frc.robot.commands.autos;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
+// import java.io.IOException;
+// import java.util.HashMap;
+// import java.util.List;
 
-import org.json.simple.parser.ParseException;
+// import org.json.simple.parser.ParseException;
 
-import frc.robot.subsystems.IntakeRoller;
-import frc.robot.subsystems.IntakeWrist;
-import frc.robot.subsystems.Elevator;
-import frc.robot.subsystems.ElevatorPivot;
-import frc.robot.subsystems.swerve.SwerveDrivetrain;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
-import com.pathplanner.lib.path.PathPlannerPath;
-import com.pathplanner.lib.util.FlippingUtil;
+// import frc.robot.subsystems.IntakeRoller;
+// import frc.robot.subsystems.IntakeWrist;
+// import frc.robot.subsystems.Elevator;
+// import frc.robot.subsystems.ElevatorPivot;
+// import frc.robot.subsystems.swerve.SwerveDrivetrain;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import edu.wpi.first.wpilibj2.command.Commands;
+// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+// import com.pathplanner.lib.auto.AutoBuilder;
+// import com.pathplanner.lib.commands.PathPlannerAuto;
+// import com.pathplanner.lib.path.PathPlannerPath;
+// import com.pathplanner.lib.util.FlippingUtil;
 
-import edu.wpi.first.math.geometry.Pose2d;
+// import edu.wpi.first.math.geometry.Pose2d;
 
-public class AutoDriving extends SequentialCommandGroup {
-    public AutoDriving(SwerveDrivetrain swerve, String autoPath) 
-    throws IOException, ParseException {
-        List<PathPlannerPath> pathGroup = PathPlannerAuto.getPathGroupFromAutoFile(autoPath);
-        Pose2d startingPose = pathGroup.get(0).getStartingDifferentialPose();
+// public class AutoDriving extends SequentialCommandGroup {
+//     public AutoDriving(SwerveDrivetrain swerve, String autoPath) 
+//     throws IOException, ParseException {
+//         List<PathPlannerPath> pathGroup = PathPlannerAuto.getPathGroupFromAutoFile(autoPath);
+//         Pose2d startingPose = pathGroup.get(0).getStartingDifferentialPose();
 
-        addCommands(
-            Commands.runOnce(() -> swerve.resetGyroFromPoseWithAlliance(startingPose)),
-            Commands.runOnce(() -> swerve.resetOdometryWithAlliance(startingPose)),
-            Commands.runOnce(swerve.getImu()::zeroAll)
-        );
+//         addCommands(
+//             Commands.runOnce(() -> swerve.resetGyroFromPoseWithAlliance(startingPose)),
+//             Commands.runOnce(() -> swerve.resetOdometryWithAlliance(startingPose)),
+//             Commands.runOnce(swerve.getImu()::zeroAll)
+//         );
 
-        for (PathPlannerPath path : pathGroup) {
-            addCommands(AutoBuilder.followPath(path));
-        }
-    }
+//         for (PathPlannerPath path : pathGroup) {
+//             addCommands(AutoBuilder.followPath(path));
+//         }
+//     }
 
-    public static Command stopDriving(IntakeRoller roller, IntakeWrist wrist, Elevator elevator, ElevatorPivot pivot) {
-        return Commands.sequence(
-            roller.stop(),
-            wrist.moveToStow(),
-            elevator.stow(),
-            pivot.moveToStow()
-        );
-    }
-}
+//     public static Command stopDriving(IntakeRoller roller, IntakeWrist wrist, Elevator elevator, ElevatorPivot pivot) {
+//         return Commands.sequence(
+//             roller.stop(),
+//             wrist.moveToStow(),
+//             elevator.stow(),
+//             pivot.moveToStow()
+//         );
+//     }
+// }
