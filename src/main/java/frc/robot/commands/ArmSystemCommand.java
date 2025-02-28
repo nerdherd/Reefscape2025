02a423ac90d3aff9dd4fed011422523278e6cc72
Jim Duan <jim.duan@skyryse.com>
2025-02-27 22:31:11 -0800

    only tie pivot and elevator

--- src/main/java/frc/robot/commands/ArmSystemCommand.java
+++ src/main/java/frc/robot/commands/ArmSystemCommand.java
@@ -3,23 +3,21 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ElevatorPivot;
+import frc.robot.Constants.ElevatorConstants;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.IntakeWrist;
 
-public class SuperSystemCommand extends Command {
+public class ArmSystemCommand extends Command {
     private final ElevatorPivot pivot;
     private final Elevator elevator;
     private final IntakeWrist wrist;
     private final double wristHeight;
     private final double wristAngle;
+    private ARM_POSITION positionID;
     private final int exeOrder;
     private final double timeoutSec;
 
     private final static int ALL_TOGETHER = 0;
-    private final static int ELV_PVT_WRT = 123;
-    private final static int PVT_ELV_WRT = 213;
-    private final static int WRT_ELV_PVT = 312;
-    private final static int WRT_PVT_ELV = 321;
 
     private boolean isStarted = false;
     private double startTime = 0;
@@ -28,25 +26,66 @@ public class SuperSystemCommand extends Command {
     double elevatorHeightRaw = 0;
     double wristAngleRaw = 0;
 
-    public SuperSystemCommand(ElevatorPivot pivot, Elevator elevator, IntakeWrist wrist,
-                              double wristHeight, double wristAngle,
+    public enum ARM_POSITION
+    {
+        STOW,
+        REEF_L1,
+        REEF_L2,
+        REEF_L3,
+        REEF_L4,
+        REEF_BALL,
+        STATION,
+        PROCESSOR,
+        CAGE,
+        FLOOR_PICK,
+        UNKNOWN
+    }
+
+    public ArmSystemCommand(ElevatorPivot pivot, Elevator elevator, IntakeWrist wrist,
+                              double wristHeight, double wristAngle, ARM_POSITION positionID,
                               int exeOrder, double timeoutSec) {
         this.pivot = pivot;
         this.elevator = elevator;
-        this.wrist = wrist;
+        this.wrist = wrist; // not for controlling wrist. 
         this.wristHeight = wristHeight;
         this.wristAngle = wristAngle;
         this.exeOrder = exeOrder;
         this.timeoutSec = timeoutSec; 
-        addRequirements(pivot, elevator, wrist);
+        this.positionID = positionID;
+        addRequirements(pivot, elevator);
     }
 
     @Override
     public void initialize() {
         isStarted = false;
         startTime = 0;
+
+        if(ALL_TOGETHER == exeOrder)
+        {
+            //enable all
+            Elevator.enabled = true;
+            ElevatorPivot.enabled = true;
+        }
+        else{
+            //TODO
+        }
+
         //calculate pivot, elevator, wrist motors position
-        updateDependencies();
+        if(positionID == ARM_POSITION.STOW)
+        {
+            pivot.setTargetPosition(0.01);
+            elevator.setPosition(0.01);
+        }
+        else if(positionID == ARM_POSITION.REEF_L1)
+        {
+            pivot.setTargetPosition(0.15);
+            elevator.setPosition(0.7);
+        }
+        else if(positionID == ARM_POSITION.REEF_L4)
+        {
+            pivot.setTargetPosition(0.2);
+            elevator.setPosition(0.9);
+        }
     }
 
     @Override
@@ -57,40 +96,12 @@ public class SuperSystemCommand extends Command {
             isStarted = true;
             startTime = Timer.getFPGATimestamp();
         }
-
-        if(ALL_TOGETHER == exeOrder)
-        {
-            //enable all
-            Elevator.enabled = true;
-            IntakeWrist.enabled = true;
-            ElevatorPivot.enabled = true;
-        }
-        else{
-            //TODO
-        }
-    }
-
-    public void updateDependencies() 
-    {
-        // TODO 
-        double p = pivot.getPositionRev();
-        double w = wrist.getPosition();
-        double e = elevator.getPosition();
-        
-        double pivotAngleRaw = wristAngle-wristAngle;
-        double elevatorHeightRaw = wristHeight-wristHeight;
-        double wristAngleRaw = wristAngle-wristAngle;
-
-        pivot.setTargetAngleRaw(pivotAngleRaw);
-        elevator.setTargetHeightRaw(elevatorHeightRaw);
-        wrist.setTargetAngleRaw(wristAngleRaw);
     }
 
     @Override
     public void end(boolean interrupted) {
         //disable all
         Elevator.enabled = false;
-        IntakeWrist.enabled = false;
         ElevatorPivot.enabled = false;
     }
 
@@ -99,8 +110,8 @@ public class SuperSystemCommand extends Command {
     {
         boolean timeout = ((Timer.getFPGATimestamp() - startTime) > timeoutSec); 
         boolean pidDone = (pivot.hasReachedPosition(pivotAngleRaw) && 
-        elevator.hasReachedPosition(elevatorHeightRaw) && 
-        wrist.hasReachedPosition(wristAngleRaw));
+        elevator.hasReachedPosition(elevatorHeightRaw) 
+        );
 
         return timeout || pidDone;
     }
