c269d37bc8075235968f725a3fb2245cd206d53a
antlikesbees <146675203+antlikesbees@users.noreply.github.com>
2025-03-07 13:41:56 -0800

    [Untested] Auto for DaVinci
    More Autos on Pathplanner + Edited Preload Taxi to include subsystem.

--- src/main/java/frc/robot/commands/autos/PreloadTaxi.java
+++ src/main/java/frc/robot/commands/autos/PreloadTaxi.java
@@ -1,7 +1,11 @@
 package frc.robot.commands.autos;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.json.simple.parser.ParseException;
+import frc.robot.Constants.SuperSystemConstants.NamedPositions;
+import frc.robot.subsystems.SuperSystem;
 import frc.robot.subsystems.swerve.SwerveDrivetrain;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -11,17 +15,26 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public class PreloadTaxi extends SequentialCommandGroup{
-    public PreloadTaxi(SwerveDrivetrain swerve, List<PathPlannerPath> pathGroup){
-        Pose2d startingPose = pathGroup.get(0).getStartingDifferentialPose();
+    public PreloadTaxi(SwerveDrivetrain swerve, String autoname, SuperSystem superSystem) throws IOException, ParseException{
 
+        List<PathPlannerPath> pathGroup = PathPlannerAuto.getPathGroupFromAutoFile(autoname);
+
+        Pose2d startingPose = pathGroup.get(0).getStartingDifferentialPose();
         addCommands(
-            Commands.runOnce(swerve.getImu()::zeroAll),
+            // Commands.runOnce(swerve.getImu()::zeroAll),
             Commands.runOnce(() -> swerve.resetGyroFromPoseWithAlliance(startingPose)),
             Commands.runOnce(() -> swerve.resetOdometryWithAlliance(startingPose)),
-            Commands.deadline(
-                AutoBuilder.followPath(pathGroup.get(0)),
-                Commands.waitSeconds(4.5)
-            )
+            Commands.sequence(
+                Commands.parallel(
+                    AutoBuilder.followPath(pathGroup.get(0)), 
+                    superSystem.moveToAuto(NamedPositions.L2)
+                ),
+                Commands.sequence(
+                    superSystem.outtake(),
+                    Commands.waitSeconds(0.2),
+                    superSystem.stopRoller()
+                )
+        )
         );
     }
 }
