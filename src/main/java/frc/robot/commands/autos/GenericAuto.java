77b4ea3f658a46c009e0642ec66fe71cc06c0bfe
billionhonk <billyhoang10@gmail.com>
2025-03-08 15:54:10 -0800

    ["UNTESTED"] Created generic auto for all piece types
    highly theoretical and needs careful testing on robot cart

--- src/main/java/frc/robot/commands/autos/GenericAuto.java
+++ src/main/java/frc/robot/commands/autos/GenericAuto.java
@@ -1,166 +1,103 @@
-// package frc.robot.commands.autos;
+package frc.robot.commands.autos;
 
-// import java.io.IOException;
-// import java.util.ArrayList;
-// import java.util.List;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-// import org.json.simple.parser.ParseException;
+import org.json.simple.parser.ParseException;
 
-// import frc.robot.subsystems.SuperSystem;
-// import frc.robot.Constants.SuperSystemConstants.NamedPositions;
-// import frc.robot.subsystems.swerve.SwerveDrivetrain;
-// import edu.wpi.first.wpilibj2.command.Command;
-// import edu.wpi.first.wpilibj2.command.Commands;
-// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-// import com.pathplanner.lib.auto.AutoBuilder;
-// import com.pathplanner.lib.commands.PathPlannerAuto;
-// import com.pathplanner.lib.path.PathPlannerPath;
-// import edu.wpi.first.math.geometry.Pose2d;
+import frc.robot.subsystems.SuperSystem;
+import frc.robot.Constants.SuperSystemConstants.NamedPositions;
+import frc.robot.subsystems.swerve.SwerveDrivetrain;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.math.geometry.Pose2d;
 
-// public class GenericAuto extends SequentialCommandGroup {
-//     private SuperSystem superSystem;
-//     private List<PathPlannerPath> pathGroup;
-//     private Pose2d startingPose;
+public class GenericAuto extends SequentialCommandGroup {
+    private SuperSystem superSystem;
+    
+    private List<PathPlannerPath> pathGroup;
+    private Pose2d startingPose;
+    private Command[] commands;
 
-//     public GenericAuto(String autoPath, SwerveDrivetrain swerve, SuperSystem superSystem, String... positions) 
-//     throws IOException, ParseException {
-//         this.superSystem = superSystem;
+    public GenericAuto(SwerveDrivetrain swerve, SuperSystem superSystem, String autoPath, int... positions) 
+    throws IOException, ParseException {
+        this.superSystem = superSystem;
 
-//         this.pathGroup = PathPlannerAuto.getPathGroupFromAutoFile(autoPath);
-//         this.startingPose = pathGroup.get(0).getStartingDifferentialPose();
+        this.pathGroup = PathPlannerAuto.getPathGroupFromAutoFile(autoPath);
+        this.startingPose = pathGroup.get(0).getStartingDifferentialPose();
+
+        // Create auto //
+        ArrayList<Command> commandsList = new ArrayList<>();
+        int pathIndex = 0;
+        for (int i = 0; i < positions.length; i++) {
+            // Go to Reef
+            commandsList.add(AutoBuilder.followPath(pathGroup.get(pathIndex++)));
+
+            // Move to branch position
+            // switch (positions[i]) {
+            //     case 1: superSystem.moveTo(NamedPositions.L1); break;
+            //     case 2: superSystem.moveTo(NamedPositions.L2); break;
+            //     case 3: superSystem.moveTo(NamedPositions.L3); break;
+            //     case 4: superSystem.moveTo(NamedPositions.L4); break;
+            // }
+            
+            // Outtake
+            commandsList.add(Commands.sequence(
+                // superSystem.outtake(),
+                Commands.waitSeconds(1.5)//,
+                // superSystem.stopRoller()
+            ));
+            
+            // If not last piece, bring coral from Station
+            if (i < positions.length-1) {
+                commandsList.add(Commands.sequence(
+                    // Go to Station
+                    // superSystem.moveTo(NamedPositions.SemiStow),
+                    AutoBuilder.followPath(pathGroup.get(pathIndex++)),
+
+                    // Move to Station and intake
+                    // superSystem.moveTo(NamedPositions.Station),
+                    // superSystem.intake(),
+                    Commands.waitSeconds(2.5),
+                    // superSystem.stopRoller(),
+
+                    // Move to Reef
+                    // superSystem.moveTo(NamedPositions.SemiStow),
+                    AutoBuilder.followPath(pathGroup.get(pathIndex++))
+                ));
+            }
+        }
+        commandsList.add(stopAuto());
+
+        commands = new Command[commandsList.size()];
+        for (int i = 0; i < commandsList.size(); i++)
+            commands[i] = commandsList.get(i);
         
-//         addCommands(
-//             Commands.runOnce(() -> swerve.resetGyroFromPoseWithAlliance(startingPose)),
-//             Commands.runOnce(() -> swerve.resetOdometryWithAlliance(startingPose)),
-//             runAuto(positions)
-//         );
-//     }
 
-//     public Command runAuto(String... positions) {
-//         ArrayList<Command> commands = new ArrayList<>();
-//         int pathIndex = 0;
-//         for (String position : positions) {
-//             commands.add(AutoBuilder.followPath(pathGroup.get(pathIndex++)));
-//             if (position.equals("L1"))
-//                 commands.add(superSystem.moveTo(NamedPositions.L1));
-//             else if (position.equals("L2"))
-//                 commands.add(superSystem.moveTo(NamedPositions.L2));
-//             else if (position.equals("L3"))
-//                 commands.add(superSystem.moveTo(NamedPositions.L3));
-//             else if (position.equals("L4"))
-//                 commands.add(superSystem.moveTo(NamedPositions.L4));
-//             commands.add(Commands.sequence(
-//                 superSystem.outtake(),
-//                 Commands.waitSeconds(1.5),
-//                 superSystem.intakeRoller.stop()
-//             ));
-//         }
+        // Return auto //
+        addCommands(
+            Commands.runOnce(() -> swerve.resetGyroFromPoseWithAlliance(startingPose)),
+            Commands.runOnce(() -> swerve.resetOdometryWithAlliance(startingPose)),
+            runAuto()
+        );
+    }
 
-//         return Commands.sequence(
-//             // Place preload
-//             AutoBuilder.followPath(pathGroup.get(0)),
-//             Commands.runOnce(() -> {
-//                 switch (positions[0]) {
-//                 case "L1": superSystem.moveTo(NamedPositions.L1); break;
-//                 case "L2": superSystem.moveTo(NamedPositions.L2); break;
-//                 case "L3": superSystem.moveTo(NamedPositions.L3); break;
-//                 case "L4": superSystem.moveTo(NamedPositions.L4); break;
-//                 }
-//             }),
-//             superSystem.outtake(),
-//             Commands.waitSeconds(1.5),
-//             superSystem.intakeRoller.stop(),
+    public Command runAuto() {
+        // Return auto //
+        return Commands.sequence(
+            commands
+        );
+    }
 
-//             // Drive to Coral Station and intake coral 2%
-//             Commands.parallel(
-//                 // elevator.stow(),
-//                 AutoBuilder.followPath(pathGroup.get(1))
-//             ),
-//             // elevator.moveToStation(),
-//             // intakeRoller.intake(),
-//             Commands.waitSeconds(2.5),
-//             superSystem.intakeRoller.stop(),
-
-//             // Drive to Reef and place coral 2
-//             Commands.parallel(
-//                 // elevator.stow(),
-//                 AutoBuilder.followPath(pathGroup.get(2))
-//             ),
-//             AutoBuilder.followPath(pathGroup.get(0)),
-//             Commands.runOnce(() -> {
-//                 switch (pos2) {
-//                 case "L1": superSystem.moveTo(NamedPositions.L1); break;
-//                 case "L2": superSystem.moveTo(NamedPositions.L2); break;
-//                 case "L3": superSystem.moveTo(NamedPositions.L3); break;
-//                 case "L4": superSystem.moveTo(NamedPositions.L4); break;
-//                 }
-//             }),
-//             superSystem.outtake(),
-//             Commands.waitSeconds(1.5),
-//             superSystem.intakeRoller.stop(),
-            
-//             // Drive to Coral Station and intake coral 3
-//             Commands.parallel(
-//                 // elevator.stow(),
-//                 AutoBuilder.followPath(pathGroup.get(3))
-//             ),
-//             // elevator.moveToStation(),
-//             // intakeRoller.intake(),
-//             Commands.waitSeconds(2.5),
-//             superSystem.intakeRoller.stop(),
-
-//             // Drive to Reef and place coral 3
-//             Commands.parallel(
-//                 // elevator.stow(),
-//                 AutoBuilder.followPath(pathGroup.get(4))
-//             ),
-//             AutoBuilder.followPath(pathGroup.get(0)),
-//             Commands.runOnce(() -> {
-//                 switch (pos3) {
-//                 case "L1": superSystem.moveTo(NamedPositions.L1); break;
-//                 case "L2": superSystem.moveTo(NamedPositions.L2); break;
-//                 case "L3": superSystem.moveTo(NamedPositions.L3); break;
-//                 case "L4": superSystem.moveTo(NamedPositions.L4); break;
-//                 }
-//             }),
-//             superSystem.outtake(),
-//             Commands.waitSeconds(1.5),
-//             superSystem.intakeRoller.stop(),
-            
-//             // Drive to Coral Station and intake coral 4
-//             Commands.parallel(
-//                 // elevator.stow(),
-//                 AutoBuilder.followPath(pathGroup.get(5))
-//             ),
-//             // elevator.moveToStation(),
-//             // intakeRoller.intake(),
-//             Commands.waitSeconds(2.5),
-//             superSystem.intakeRoller.stop(),
-
-//             // Drive to Reef and place coral 4
-//             Commands.parallel(
-//                 // elevator.stow(),
-//                 AutoBuilder.followPath(pathGroup.get(6))
-//             ),
-//             AutoBuilder.followPath(pathGroup.get(0)),
-//             Commands.runOnce(() -> {
-//                 switch (pos1) {
-//                 case "L1": superSystem.moveTo(NamedPositions.L1); break;
-//                 case "L2": superSystem.moveTo(NamedPositions.L2); break;
-//                 case "L3": superSystem.moveTo(NamedPositions.L3); break;
-//                 case "L4": superSystem.moveTo(NamedPositions.L4); break;
-//                 }
-//             }),
-//             superSystem.outtake(),
-//             Commands.waitSeconds(1.5),
-//             stopAuto()
-//         );
-//     }
-
-//     public Command stopAuto() {
-//         return Commands.sequence(
-//             superSystem.intakeRoller.stop()//,
-//             // elevator.stow()
-//         );
-//     }
-// }
+    public Command stopAuto() {
+        return Commands.sequence(
+            superSystem.stopRoller()//,
+            // elevator.stow()
+        );
+    }
+}
