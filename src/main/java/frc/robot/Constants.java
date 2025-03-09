6436d0dfc00a9f969ace72846c6deffc8a46603e
Jim Duan <jim.duan@skyryse.com>
2025-03-09 16:12:21 -0700

    rename cameras

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -227,14 +227,14 @@ public final class Constants {
 
   public static final class VisionConstants {
 
-    public static final String kLimelightLowLeftName = "limelight-lowleft";
-    public static final String kLimelightLowLeftIP = "10.6.87.12:5802";
-    public static final String kLimelightLowRightName = "limelight-lowright";
-    public static final String kLimelightLowRightIP = "10.6.87.24:5802";
-    public static final String kLimelightHighLeftName = "limelight-highleft";
-    public static final String kLimelightHighLeftIP = "10.6.87.55:5802";
-    public static final String kLimelightHighRightName = "limelight-highright";
-    public static final String kLimelightHighRightIP = "10.6.87.75:5802";
+    public static final String kLimelightLowLeftName = "limelight-fl";
+    public static final String kLimelightLowLeftIP = "10.6.87.5:5802";
+    public static final String kLimelightLowRightName = "limelight-fr";
+    public static final String kLimelightLowRightIP = "10.6.87.7:5802";
+    public static final String kLimelightHighLeftName = "limelight-bl";
+    public static final String kLimelightHighLeftIP = "10.6.87.15:5802";
+    public static final String kLimelightHighRightName = "limelight-br";
+    public static final String kLimelightHighRightIP = "10.6.87.17:5802";
 
     public static final double kFrontCameraHeightMeters = 0; // TODO change for new bot limelights
     public static final double kNoteHeightMeters = 0;
