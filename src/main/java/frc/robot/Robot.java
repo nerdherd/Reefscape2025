389183dc7b0e75678124cfe98da0836e9919d364
jim.duan@skyryse.com <jim.duan@skyryse.com>
2025-03-12 17:07:31 -0700

    stop motions for motor during TestMode

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -142,7 +142,11 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+    m_robotContainer.elevator.stopMotion();
+    m_robotContainer.elevatorPivot.stopMotion();
+    m_robotContainer.intakeWrist.stopMotion();
+  }
 
   /** This function is called once when the robot is first started up. */
   @Override
