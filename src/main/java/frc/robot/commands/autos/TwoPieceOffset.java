52fa3e3400987cb7280527b06ce422d130a29415
Zachary Miller <superz006@icloud.com>
2025-03-14 19:29:29 -0700

    [Working] Path updates, climb neutral mode

---
+++ src/main/java/frc/robot/commands/autos/TwoPieceOffset.java
@@ -0,0 +1,81 @@
+package frc.robot.commands.autos;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.json.simple.parser.ParseException;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.SuperSystemConstants.NamedPositions;
+import frc.robot.subsystems.SuperSystem;
+import frc.robot.subsystems.swerve.SwerveDrivetrain;
+
+
+public class TwoPieceOffset extends SequentialCommandGroup {
+    public TwoPieceOffset(SwerveDrivetrain swerve, String autoname, SuperSystem superSystem) throws IOException, ParseException {
+        
+        List<PathPlannerPath> pathGroup = PathPlannerAuto.getPathGroupFromAutoFile(autoname);
+
+        addCommands(
+            Commands.runOnce(swerve.getImu()::zeroAll), //Check if needed
+            // Commands.runOnce(() -> swerve.getImu().setOffset(startingPose.getRotation().getDegrees())),
+            // Commands.runOnce(()->swerve.resetOdometryWithAlliance(startingPose)),
+            
+            Commands.sequence(
+                Commands.sequence(
+                    superSystem.holdPiece(),
+                    AutoBuilder.followPath(pathGroup.get(0)),
+                    Commands.sequence(
+                        superSystem.moveToAuto(NamedPositions.L4),
+                        AutoBuilder.followPath(pathGroup.get(1))
+                    )
+                ),
+                Commands.sequence(
+                    superSystem.outtake(),
+                    Commands.waitSeconds(0.3),
+                    superSystem.stopRoller()
+                ),
+                Commands.sequence(
+                    superSystem.moveTo(NamedPositions.L5),
+                    Commands.parallel(
+                        AutoBuilder.followPath(pathGroup.get(2)),
+                        superSystem.moveToAuto(NamedPositions.Station)
+                    )
+                ),
+                Commands.sequence(
+                    // superSystem.intake(),
+                    superSystem.intakeUntilSensed(1.5),
+                    // Commands.waitSeconds(2),
+                    superSystem.holdPiece()
+                ),
+                Commands.sequence(
+                    Commands.parallel(
+                        Commands.sequence(
+                            Commands.waitSeconds(0.3)
+                        ),
+                        AutoBuilder.followPath(pathGroup.get(3))
+                    ),
+                    Commands.sequence(
+                        superSystem.moveToAuto(NamedPositions.L4),
+                        AutoBuilder.followPath(pathGroup.get(4))
+                    )
+                ),
+                Commands.sequence(
+                    superSystem.outtake(),
+                    Commands.waitSeconds(0.5),
+                    superSystem.stopRoller()
+                ),
+
+                Commands.sequence(
+                    superSystem.moveTo(NamedPositions.L5),
+                    superSystem.moveTo(NamedPositions.SemiStow)
+                )
+            )
+        );
+    }
+}
