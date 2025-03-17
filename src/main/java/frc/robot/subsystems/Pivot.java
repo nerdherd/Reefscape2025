b7c514f78d6cc823783d038117c2f4f76ba03e67
Zachary Miller <93176330+londonwafflez@users.noreply.github.com>
2025-03-16 22:01:42 -0700

    Big naming changes and file deletions continued

---
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -0,0 +1,342 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.PivotConstants;
+import frc.robot.util.NerdyMath;
+
+public class Pivot extends SubsystemBase implements Reportable{
+    private TalonFX pivotMotor;
+    private TalonFX pivotMotorRight;
+
+    private TalonFXConfigurator pivotConfigurator;
+    private TalonFXConfigurator pivotConfiguratorRight; 
+    private Pigeon2 pigeon;
+    private double elevatorPosition;
+
+    public boolean enabled = false; 
+    private double desiredPosition; 
+    private final MotionMagicVoltage motionMagicRequest;  
+    private final NeutralOut brakeRequest = new NeutralOut();
+    private NeutralModeValue neutralMode = NeutralModeValue.Brake;
+
+    private final Follower followRequest = new Follower(PivotConstants.kLeftPivotMotorID, true);
+    // public final VoltageOut voltageRequest = new VoltageOut(0);
+
+    private double ff;
+    private double ffInverse;
+
+    private double commandedVoltage = 0.0;
+
+    public Pivot () {
+        desiredPosition = 0.0;
+        motionMagicRequest = new MotionMagicVoltage(desiredPosition);
+
+        pivotMotor = new TalonFX(PivotConstants.kLeftPivotMotorID);
+        pivotConfigurator = pivotMotor.getConfigurator();
+
+        pivotMotorRight = new TalonFX(PivotConstants.kRightPivotMotorID);
+        // pigeon = new Pigeon2(V1ElevatorConstants.kPivotPigeonID); // Not using Pigeon as of 2/23
+
+        pivotConfiguratorRight = pivotMotorRight.getConfigurator();
+
+        configureMotorV1();
+        configurePIDV1();
+        
+        zeroEncoder();
+        CommandScheduler.getInstance().registerSubsystem(this);
+    }
+    
+    // ******************************** SETUP METHODS *************************************** //
+    private void configurePIDV1() {
+        TalonFXConfiguration pivotConfiguration = new TalonFXConfiguration();
+        
+        pivotConfigurator.refresh(pivotConfiguration);
+
+        pivotConfiguration.Slot0.kP = PivotConstants.kPElevatorPivot; 
+        pivotConfiguration.Slot0.kI = PivotConstants.kIElevatorPivot;
+        pivotConfiguration.Slot0.kD = PivotConstants.kDElevatorPivot;
+        pivotConfiguration.Slot0.kV = PivotConstants.kVElevatorPivot;
+        pivotConfiguration.Slot0.kS = PivotConstants.kSElevatorPivot;
+        pivotConfiguration.Slot0.kA = PivotConstants.kAElevatorPivot;
+        pivotConfiguration.Slot0.kG = PivotConstants.kGElevatorPivot;
+        
+        pivotConfiguration.MotionMagic.MotionMagicCruiseVelocity = PivotConstants.kEPivotCruiseVelocity;
+        pivotConfiguration.MotionMagic.MotionMagicAcceleration = PivotConstants.kElevatorPivotCruiseAcceleration;
+        pivotConfiguration.MotionMagic.MotionMagicJerk = PivotConstants.kElevatorPivotJerk;
+        pivotConfiguration.MotionMagic.MotionMagicExpo_kV = 0;
+        pivotConfiguration.MotionMagic.MotionMagicExpo_kA = 0;
+        
+        TalonFXConfiguration pivotConfigurationRight = new TalonFXConfiguration();
+
+        pivotConfiguratorRight.refresh(pivotConfigurationRight);
+
+        pivotConfigurationRight.Slot0.kP = PivotConstants.kPElevatorPivot; 
+        pivotConfigurationRight.Slot0.kI = PivotConstants.kIElevatorPivot;
+        pivotConfigurationRight.Slot0.kD = PivotConstants.kDElevatorPivot;
+        pivotConfigurationRight.Slot0.kV = PivotConstants.kVElevatorPivot;
+        pivotConfigurationRight.Slot0.kS = PivotConstants.kSElevatorPivot;
+        pivotConfigurationRight.Slot0.kA = PivotConstants.kAElevatorPivot;
+        pivotConfigurationRight.Slot0.kG = PivotConstants.kGElevatorPivot;
+        
+        pivotConfigurationRight.MotionMagic.MotionMagicCruiseVelocity = PivotConstants.kEPivotCruiseVelocity;
+        pivotConfigurationRight.MotionMagic.MotionMagicAcceleration = PivotConstants.kElevatorPivotCruiseAcceleration;
+        // pivotConfigurationRight.MotionMagic.MotionMagicJerk = V1ElevatorConstants.kElevatorPivotJerk; // TODO
+        pivotConfigurationRight.MotionMagic.MotionMagicExpo_kV = 0;
+        pivotConfigurationRight.MotionMagic.MotionMagicExpo_kA = 0;
+        
+        StatusCode statusCode = pivotConfigurator.apply(pivotConfiguration);
+        if(!statusCode.isOK()){
+            DriverStation.reportError(" Could not apply elevator pivot configs, error code =(", true);
+        }
+
+        StatusCode statusCodeRight = pivotConfiguratorRight.apply(pivotConfigurationRight);
+        if(!statusCodeRight.isOK()){
+            DriverStation.reportError(" Could not apply elevator RIGHT pivot configs, error code =(", true);
+        }
+    }
+
+    public void configureMotorV1() {
+        TalonFXConfiguration pivotConfiguration = new TalonFXConfiguration();
+        
+        pivotConfigurator.refresh(pivotConfiguration);
+        // pivotConfiguration.Feedback.FeedbackRemoteSensorID = FeedbackSensorSourceValue.RotorSensor;
+        pivotConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor; 
+        // pivotConfiguration.Feedback.RotorToSensorRatio = ; // 0.1
+        pivotConfiguration.Feedback.SensorToMechanismRatio = PivotConstants.kElevatorPivotGearRatio; 
+        pivotConfiguration.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; 
+        pivotConfiguration.Voltage.PeakForwardVoltage = 11.5;
+        pivotConfiguration.Voltage.PeakReverseVoltage = -11.5;
+        pivotConfiguration.CurrentLimits.SupplyCurrentLimit = 40;
+        pivotConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
+        pivotConfiguration.CurrentLimits.StatorCurrentLimit = 100;
+        pivotConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
+        pivotConfiguration.Audio.AllowMusicDurDisable = true;
+        pivotConfiguration.MotorOutput.NeutralMode = neutralMode;
+
+        StatusCode statusCode = pivotConfigurator.apply(pivotConfiguration);
+        if (!statusCode.isOK()){
+            DriverStation.reportError("Could not apply Elevator configs, fix code??? =(", true);
+        }
+
+        TalonFXConfiguration pivotConfigurationRight = new TalonFXConfiguration();
+
+        pivotConfiguratorRight.refresh(pivotConfigurationRight);
+        // pivotConfigurationRight.Feedback.FeedbackRemoteSensorID = V1ElevatorConstants.kPivotPigeonID;
+        pivotConfigurationRight.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor; //TODO change orientation later
+        // pivotConfigurationRight.Feedback.RotorToSensorRatio = V1ElevatorConstants.kElevatorPivotGearRatio;
+        pivotConfigurationRight.Feedback.SensorToMechanismRatio = PivotConstants.kElevatorPivotGearRatio; 
+        pivotConfigurationRight.MotorOutput.Inverted = InvertedValue.Clockwise_Positive; 
+        pivotConfigurationRight.Voltage.PeakForwardVoltage = 11.5;
+        pivotConfigurationRight.Voltage.PeakReverseVoltage = -11.5;
+        pivotConfigurationRight.CurrentLimits.SupplyCurrentLimit = 40;
+        pivotConfigurationRight.CurrentLimits.SupplyCurrentLimitEnable = false;
+        pivotConfigurationRight.CurrentLimits.StatorCurrentLimit = 100;
+        pivotConfigurationRight.CurrentLimits.StatorCurrentLimitEnable = false;
+        pivotConfigurationRight.Audio.AllowMusicDurDisable = true;
+        pivotConfigurationRight.MotorOutput.NeutralMode = neutralMode;
+
+        StatusCode RightstatusCode = pivotConfiguratorRight.apply(pivotConfigurationRight);
+        if (!RightstatusCode.isOK()){
+            DriverStation.reportError("Could not apply Elevator configs, fix code??? =(", true);
+        }
+    }
+
+    @Override
+    public void periodic() {
+        // why do we change it? -Duan
+        //ff = (ElevatorConstants.kElevatorPivotStowedFF + ElevatorConstants.kElevatorPivotDiffFF * (elevatorPosition / ElevatorConstants.kElevatorPivotExtendedFFPosition)) * Math.cos(2 * Math.PI * getPosition());
+        ff =  PivotConstants.kFElevatorPivot * Math.cos(2 * Math.PI * getPosition());
+
+        if (enabled) {
+            pivotMotor.setControl(motionMagicRequest.withFeedForward(ff)); 
+
+            pivotMotorRight.setControl(followRequest); 
+        }
+        else {
+            pivotMotor.setControl(brakeRequest);
+        }
+    }
+
+    // ****************************** STATE METHODS ***************************** //
+    public void zeroEncoder() {
+        pivotMotor.setPosition(0);
+        pivotMotorRight.setPosition(0);
+        desiredPosition = 0.0;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public void setNeutralMode(NeutralModeValue neutralMode) {
+        this.neutralMode = neutralMode;
+    }
+
+    public void stopMotion() {
+        pivotMotor.setControl(brakeRequest);
+        pivotMotorRight.setControl(brakeRequest);
+    }
+
+    public void setTargetPosition(double position) {
+        //TODO NerdyMath.clamp(
+        desiredPosition = position;
+        motionMagicRequest.Position = desiredPosition;
+    }
+
+    public void setPivotVoltage(double voltage) {
+        //TODO NerdyMath.clamp(
+        commandedVoltage = voltage;
+        pivotMotor.setVoltage(commandedVoltage);
+    }
+
+    public boolean atPosition() {
+        return NerdyMath.inRange(pivotMotor.getPosition().getValueAsDouble(), 
+        desiredPosition - 0.01,
+        desiredPosition + 0.01);
+    }
+    
+    public boolean atPositionWide() {
+        return NerdyMath.inRange(pivotMotor.getPosition().getValueAsDouble(), 
+        desiredPosition - 0.04,
+        desiredPosition + 0.04);
+    }
+
+    // private void setPositionDegrees(double positionDegrees) {
+    //     double newPos = NerdyMath.clamp(
+    //         positionDegrees, 
+    //         ElevatorConstants.kElevatorPivotMin, 
+    //         ElevatorConstants.kElevatorPivotMax
+    //     );
+
+    //     motionMagicRequest.Position = (newPos / 360.0);  
+    // }
+
+    // private void incrementPosition(double incrementDegrees) {
+    //     if(Math.abs(incrementDegrees) <= 0.001) {
+    //         return;
+    //     }
+    //     setPositionDegrees(getTargetPositionDegrees() + incrementDegrees);
+    // }
+
+
+    // private double getTargetPositionRev() {
+    //     return motionMagicRequest.Position;
+    // }
+
+    // private double getTargetPositionDegrees() {
+    //     return getTargetPositionRev() * 360;
+    // }
+
+    public double getPosition() {
+        ///////////////////
+        /// TODO: we do need the offset for pivot. because current "reset 0" is not the real horizontal zero
+        /// ????
+        return pivotMotor.getPosition().getValueAsDouble(); 
+    }
+
+    
+
+    public double getPositionDegrees() {
+        return getPosition() * 360;
+    }
+
+    public void setElevatorLength(double len) {
+        // TODO
+    }
+
+    // ****************************** COMMAND METHODS ***************************** //
+
+    // public Object resetEncoders() { // Reset both pivot encoders to 0. Can delete after use
+    //     pivotMotor.setSelectedSensorPosition(0);
+
+    // }
+
+    public Command setEnabledCommand(boolean enabled) {
+        return Commands.runOnce(() -> setEnabled(enabled));
+    }
+
+    public Command stopCommand() {
+        return Commands.sequence(
+            Commands.runOnce(() -> pivotMotor.setControl(brakeRequest)),
+            setEnabledCommand(false)
+        );
+    }
+
+    public Command stop() {
+        return stopCommand();
+    }
+
+    // ****************************** LOGGING METHODS ****************************** //
+
+    @Override
+    public void reportToSmartDashboard(LOG_LEVEL priority) {
+        SmartDashboard.putNumber("Pivot Voltage (ID 17)", pivotMotor.getMotorVoltage().getValueAsDouble());
+        SmartDashboard.putNumber("Pivot Voltage (ID 18)", pivotMotorRight.getMotorVoltage().getValueAsDouble());
+
+        SmartDashboard.putNumber("Pivot Current Rotations (ID 17)", pivotMotor.getPosition().getValueAsDouble());
+        // SmartDashboard.putNumber("Pivot Current Rotations (ID 18)", pivotMotorRight.getPosition().getValueAsDouble());
+        SmartDashboard.putNumber("Pivot Target Rotations", desiredPosition);
+        // SmartDashboard.putNumber("Error Left (Rotations)", error);
+        
+
+        // SmartDashboard.putNumber("Adjusted Offset Pivot Current Rotations (ID 17)", getPositionRev());
+        // SmartDashboard.putNumber("Adjusted Offset Pivot Commanded Rotations (ID 17)", desiredPosition - pivotPositionOffset);
+        // SmartDashboard.putNumber("Commanded kP Pivot Voltage", commandedVoltage);
+
+        // SmartDashboard.putNumber("Applied feedforward", ff);
+        SmartDashboard.putNumber("Feedforward inverse", ffInverse);
+
+        // SmartDashboard.putNumber("Error Right (Rotations)", errorRight);
+
+    }
+    
+    @Override
+    public void initShuffleboard(LOG_LEVEL priority) {
+        if (priority == LOG_LEVEL.OFF) {
+            return;
+        }
+        ShuffleboardTab tab = Shuffleboard.getTab("Elevator Pivot");
+        switch (priority) {
+            case OFF:
+                break;
+            case ALL:
+                tab.addString("Pivot Control Mode", pivotMotor.getControlMode()::toString);
+                tab.addNumber("Pivot MM Position", () -> motionMagicRequest.Position);
+                tab.addNumber("Pivot FF", () -> motionMagicRequest.FeedForward);
+                tab.addNumber("Pivot Supply Current", () -> pivotMotor.getSupplyCurrent().getValueAsDouble());
+            case MEDIUM:
+                tab.addBoolean("Pivot Enabled", () -> enabled);
+                tab.addNumber("Pivot Desired Position", ()-> desiredPosition);
+                tab.addNumber("Pivot Current Position", () -> getPosition());
+                tab.addBoolean("Pivot At Position", () -> atPosition());
+            case MINIMAL:
+                tab.addNumber("Pivot Voltage", () -> pivotMotor.getMotorVoltage().getValueAsDouble());    
+                tab.addNumber("Pivot Temperature 1", () -> pivotMotor.getDeviceTemp().getValueAsDouble());
+                tab.addNumber("Pivot Temperature 2", () -> pivotMotorRight.getDeviceTemp().getValueAsDouble());
+                
+                break;
+            }
+    }
+    
+}
---
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -0,0 +1,266 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.WristConstants;
+import frc.robot.util.NerdyMath;;
+
+public class Wrist extends SubsystemBase implements Reportable{
+    private final TalonFX motor;
+    private final TalonFXConfigurator motorConfigurator;
+    private Pigeon2 pigeon;
+
+    private final MotionMagicVoltage motionMagicRequest; // Was 0 during initialization
+    private final NeutralOut brakeRequest = new NeutralOut();
+
+    private double desiredPosition; // Should be ~90 or wherever initial position is
+    private double desiredAngle;
+    private boolean enabled = false;
+    private boolean V1 = true;
+    private double ff = 0;
+    private double pivotAngle = 0;
+    public TalonFXConfiguration motorConfigs;
+
+    private NeutralModeValue neutralMode = NeutralModeValue.Brake;
+
+    public Wrist(boolean V1) {
+        this.V1 = V1;
+        motor = new TalonFX(WristConstants.kMotorID);
+        motorConfigurator = motor.getConfigurator();
+        
+        // TODO: Took out immediate stow to work on Wrist tuning
+
+        if(V1) {
+            // pigeon = new Pigeon2(WristConstants.kPigeonID, "rio");
+            desiredPosition = 0;
+            // desiredPosition = pigeon.getRoll().getValueAsDouble();
+            motionMagicRequest = new MotionMagicVoltage(desiredPosition);
+        }
+        else {
+            pigeon = new Pigeon2(WristConstants.kPigeonID, "rio");
+            // desiredPosition = 89;
+            desiredPosition = pigeon.getRoll().getValueAsDouble();
+
+
+            motionMagicRequest = new MotionMagicVoltage(desiredPosition);
+        }
+        motor.setControl(brakeRequest);
+
+        // configure motor
+         motorConfigs = new TalonFXConfiguration();
+        configurePID(motorConfigs);
+        
+        zeroEncoder();
+        CommandScheduler.getInstance().registerSubsystem(this);
+    }
+
+    //****************************** SETUP METHODS ******************************//
+
+    public void configurePID(TalonFXConfiguration motorConfigs) {
+        if (V1){
+            motorConfigurator.refresh(motorConfigs);
+
+            // motorConfigs.Feedback.FeedbackRemoteSensorID = V1IntakeConstants.kPigeonID;
+            motorConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+            motorConfigs.Feedback.SensorToMechanismRatio = 13.89; 
+            // motorConfigs.Feedback.RotorToSensorRatio;
+            motorConfigs.CurrentLimits.SupplyCurrentLimit = 40;
+            motorConfigs.CurrentLimits.SupplyCurrentLimitEnable = true;
+            motorConfigs.CurrentLimits.SupplyCurrentLowerLimit = 45;
+            motorConfigs.CurrentLimits.SupplyCurrentLowerTime = 0.1;
+            motorConfigs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        
+            motorConfigs.Slot0.kP = WristConstants.kPMotor;
+            motorConfigs.Slot0.kI = WristConstants.kItMotor;
+            motorConfigs.Slot0.kD = WristConstants.kDMotor;
+            motorConfigs.Slot0.kV = WristConstants.kVMotor;
+            motorConfigs.Slot0.kS = WristConstants.kSMotor;
+            motorConfigs.Slot0.kG = WristConstants.kGMotor;
+
+            motorConfigs.MotionMagic.MotionMagicCruiseVelocity =  WristConstants.kCruiseVelocity;
+            motorConfigs.MotionMagic.MotionMagicAcceleration = WristConstants.kAcceleration;
+            motorConfigs.MotionMagic.MotionMagicJerk = WristConstants.kJerk;
+        
+            motorConfigs.MotorOutput.NeutralMode = neutralMode;
+
+            StatusCode response = motorConfigurator.apply(motorConfigs);
+            if (!response.isOK()){
+                DriverStation.reportError("Could not apply motor configs, error code:" + response.toString(), new Error().getStackTrace());
+            }
+        } else {
+            motorConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+            motorConfigs.Feedback.SensorToMechanismRatio = 12.0/54.0;
+            motorConfigs.CurrentLimits.SupplyCurrentLimit = 25;
+            motorConfigs.CurrentLimits.SupplyCurrentLimitEnable = true;
+            motorConfigs.CurrentLimits.SupplyCurrentLowerLimit = 30;
+            motorConfigs.CurrentLimits.SupplyCurrentLowerTime = 0.1;
+            motorConfigs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        
+            motorConfigs.Slot0.kP = WristConstants.kPMotor;
+            motorConfigs.Slot0.kI = WristConstants.kItMotor;
+            motorConfigs.Slot0.kD = WristConstants.kDMotor;
+            motorConfigs.Slot0.kV = WristConstants.kVMotor;
+            motorConfigs.Slot0.kS = WristConstants.kSMotor;
+            // motorConfigs.Slot0.kG = WristConstants.kGMotor; kG applied in ff
+    
+            motorConfigs.MotionMagic.MotionMagicAcceleration = WristConstants.kAcceleration;
+            motorConfigs.MotionMagic.MotionMagicJerk = WristConstants.kJerk;
+        
+            StatusCode response = motorConfigurator.apply(motorConfigs);
+            if (!response.isOK()){
+                DriverStation.reportError("Could not apply motor configs, error code:" + response.toString(), new Error().getStackTrace());
+            }
+        }
+    }
+
+    @Override
+    public void periodic() {
+        if(!enabled) {
+            motor.setControl(brakeRequest);
+            return;
+        }
+
+        // desiredPosition + pivot * constantToChangeUnit
+        // ff = (-3.2787 * desiredPosition) - 1.5475; Harder method
+        ff = WristConstants.kGMotor * Math.cos((getPosition() + 0.54 + pivotAngle) * 2 * Math.PI); // 0.5437 is wrist horizontal 
+        motor.setControl(motionMagicRequest.withFeedForward(ff));
+    }
+
+    // ****************************** STATE METHODS ****************************** //
+
+    public void setEnabled(boolean e) {
+        this.enabled = e;
+    }
+
+    public void stopMotion() {
+        motor.setControl(brakeRequest);
+    }
+    
+    public void setNeutralMode(NeutralModeValue neutralMode) {
+        this.neutralMode = neutralMode;
+    }
+    
+    public void setTargetPosition(double position) {
+        //TODO NerdyMath.clamp(
+        desiredPosition = position;
+        motionMagicRequest.Position = desiredPosition;
+    }
+
+    // public void setPositionDegrees(double positionDegrees) {
+    //     desiredAngle = positionDegrees;
+    //     desiredPosition = (desiredAngle * 0.015432) - 0.00011; // Depricated values
+        
+    //     // double newPos = NerdyMath.clamp(
+    //     //     positionDegrees, 
+
+    //     // );
+
+    //     motionMagicRequest.Position = (desiredPosition);  // = (newPos / 360.0)
+    // }
+
+    public void zeroEncoder() {
+        motor.setPosition(0);
+        desiredPosition = 0;
+    }
+
+    public void setPivotAngle(double pivotAngle) {
+        this.pivotAngle = pivotAngle;
+    }
+
+    public double getPosition() {
+        return motor.getPosition().getValueAsDouble();
+    }
+
+    public boolean atPosition() {
+        return NerdyMath.inRange(motor.getPosition().getValueAsDouble(), 
+                                desiredPosition - 0.05,
+                                desiredPosition + 0.05);
+    }
+
+    public boolean atPositionWide() {
+        return NerdyMath.inRange(motor.getPosition().getValueAsDouble(), 
+                                desiredPosition - 0.15,
+                                desiredPosition + 0.15);
+    }
+
+    // ****************************** COMMAND METHODS ****************************** //
+    public Command setEnabledCommand(boolean enabled) {
+        return Commands.runOnce(() -> this.setEnabled(enabled));
+    }
+
+    public Command setPositionCommand(double position) {
+        return Commands.sequence(
+            Commands.runOnce(() -> setTargetPosition(position))
+        );
+    }
+
+    public Command stopCommand() {
+        return Commands.sequence(
+            setEnabledCommand(false),
+            Commands.runOnce(() -> motor.setControl(brakeRequest))
+        );
+    }
+
+    public Command setPivotAngleCommand(double pivotAngle) {
+        return Commands.runOnce(() -> setPivotAngle(pivotAngle));
+    }
+
+    // ****************************** LOGGING METHODS ****************************** //
+
+    @Override
+    public void reportToSmartDashboard(LOG_LEVEL level) {
+        switch (level) {
+            case OFF:
+                break;
+            case ALL:
+                SmartDashboard.putNumber("Coral Wrist Position", motor.getPosition().getValueAsDouble());
+            case MEDIUM:
+                SmartDashboard.putNumber("Coral Wrist Current", motor.getStatorCurrent().getValueAsDouble());
+            case MINIMAL:
+                break;
+        }
+    }
+    
+    @Override
+    public void initShuffleboard(LOG_LEVEL priority) { 
+        if (priority == LOG_LEVEL.OFF) {
+            return;
+        }
+        ShuffleboardTab tab = Shuffleboard.getTab("Intake Wrist");
+        switch (priority) {
+            case OFF:
+                break;
+            case ALL:
+                tab.addString("Wrist Control Mode", motor.getControlMode()::toString);
+                tab.addBoolean("Wrist At Position", () -> atPosition());
+                tab.addNumber("Wrist FF", () -> motionMagicRequest.FeedForward);
+            case MEDIUM:
+                tab.addNumber("Wrist Supply Current", () -> motor.getSupplyCurrent().getValueAsDouble());
+            case MINIMAL:
+                tab.addNumber("Wrist Temperature", () -> motor.getDeviceTemp().getValueAsDouble());
+                tab.addNumber("Wrist Desired Position", () -> desiredPosition);
+                tab.addNumber("Wrist Current Position", () -> motor.getPosition().getValueAsDouble());
+                tab.addNumber("Wrist Voltage", () -> motor.getMotorVoltage().getValueAsDouble());
+                break;
+        }
+    }
+}
