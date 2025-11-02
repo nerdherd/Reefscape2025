61787cea2186c749b8211cd65ecf62a4c2259304
nerdherd <billyhoang10@gmail.com>
2025-11-02 14:16:50 -0800

    reset pose on auto init

--- src/main/java/frc/robot/commands/autos/PreloadTaxi.java
+++ src/main/java/frc/robot/commands/autos/PreloadTaxi.java
@@ -7,6 +7,7 @@ import org.json.simple.parser.ParseException;
 import frc.robot.Constants.SuperSystemConstants.PositionEquivalents;
 import frc.robot.subsystems.SuperSystem;
 import frc.robot.subsystems.swerve.SwerveDrivetrain;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import com.pathplanner.lib.auto.AutoBuilder;
@@ -17,16 +18,18 @@ public class PreloadTaxi extends SequentialCommandGroup{
     public PreloadTaxi(String autoname, SuperSystem superSystem, SwerveDrivetrain swerve) throws IOException, ParseException{
 
         List<PathPlannerPath> pathGroup = PathPlannerAuto.getPathGroupFromAutoFile(autoname);
-        // Pose2d startingPose = pathGroup.get(0).getStartingDifferentialPose();
+        Pose2d startingPose = pathGroup.get(0).getStartingDifferentialPose();
         
         addCommands(
-            Commands.runOnce(swerve.getImu()::zeroAll),
+            // Commands.runOnce(() -> swerve.resetGyroFromPoseWithAlliance(startingPose)),
+            Commands.runOnce(() -> swerve.resetOdometryWithAlliance(startingPose)),
+            // Commands.runOnce(swerve.getImu()::zeroAll),
             
             Commands.sequence(
                 AutoBuilder.followPath(pathGroup.get(0)),
                 Commands.waitSeconds(0.5),
                 superSystem.moveToAuto(PositionEquivalents.SemiStow),
-                superSystem.moveToAuto(PositionEquivalents.L4),
+                superSystem.moveToAuto(PositionEquivalents.L1),
                 Commands.waitSeconds(1),
 
                 superSystem.outtake(),
@@ -34,7 +37,7 @@ public class PreloadTaxi extends SequentialCommandGroup{
                 superSystem.stopRoller(),
                 Commands.waitSeconds(1),
 
-                superSystem.moveToAuto(PositionEquivalents.L2),
+                superSystem.moveToAuto(PositionEquivalents.L1),
                 superSystem.moveToAuto(PositionEquivalents.SemiStow)
             )
         );
--- src/main/java/frc/robot/commands/autos/TwoPieceGround.java
+++ src/main/java/frc/robot/commands/autos/TwoPieceGround.java
@@ -8,6 +8,8 @@ import org.json.simple.parser.ParseException;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.SuperSystemConstants.PositionEquivalents;
@@ -19,11 +21,12 @@ public class TwoPieceGround extends SequentialCommandGroup {
     public TwoPieceGround(String autoname, SuperSystem superSystem, SwerveDrivetrain swerve) throws IOException, ParseException {
         
         List<PathPlannerPath> pathGroup = PathPlannerAuto.getPathGroupFromAutoFile(autoname);
-        // Pose2d startingPose = pathGroup.get(0).getStartingDifferentialPose();
-
+        Pose2d startingPose = pathGroup.get(0).getStartingDifferentialPose();
+        
         addCommands(
-            Commands.runOnce(swerve.getImu()::zeroAll),
-            Commands.waitSeconds(0.5),
+            // Commands.runOnce(() -> swerve.resetGyroFromPoseWithAlliance(startingPose)),
+            Commands.runOnce(() -> swerve.resetOdometryWithAlliance(startingPose)),
+            // Commands.runOnce(swerve.getImu()::zeroAll),
             
             Commands.sequence(
                 // Move to Reef
@@ -33,22 +36,22 @@ public class TwoPieceGround extends SequentialCommandGroup {
                         superSystem.moveToAuto(PositionEquivalents.SemiStow)                        // superSystem.moveToAuto(PositionEquivalents.L2)
                     )
                 ),
-                superSystem.moveToAuto(PositionEquivalents.L4),
-                Commands.waitSeconds(1),
+                superSystem.moveToAuto(PositionEquivalents.L1),
+                Commands.waitSeconds(0.5),
 
                 // Outtake
                 superSystem.outtake(),
                 Commands.waitSeconds(1),
                 superSystem.stopRoller(),
-                superSystem.moveToAuto(PositionEquivalents.L2),
+                superSystem.moveToAuto(PositionEquivalents.L1),
 
                 // Move to A3O
                 AutoBuilder.followPath(pathGroup.get(1)),
                 Commands.sequence(
                     superSystem.moveToAuto(PositionEquivalents.SemiStow),
-                    superSystem.moveToAuto(PositionEquivalents.GroundIntake)
+                    superSystem.moveToAuto(PositionEquivalents.Stow)
                 ),
-                Commands.waitSeconds(1),
+                Commands.waitSeconds(0.1),
 
                 // Move to and intake ground coral
                 Commands.parallel(
@@ -64,14 +67,14 @@ public class TwoPieceGround extends SequentialCommandGroup {
                         superSystem.moveToAuto(PositionEquivalents.SemiStow)
                     )
                 ),
-                superSystem.moveToAuto(PositionEquivalents.L4),
+                superSystem.moveToAuto(PositionEquivalents.L1),
                 Commands.waitSeconds(1),
 
                 // Outtake
                 superSystem.outtake(),
                 Commands.waitSeconds(1),
                 superSystem.stopRoller(),
-                superSystem.moveToAuto(PositionEquivalents.L2),
+                superSystem.moveToAuto(PositionEquivalents.L1),
 
                 // Prepare for teleop
                 Commands.parallel(
